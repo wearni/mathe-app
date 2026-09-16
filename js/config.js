d5e2eddd42576db097a0aa72aef908619865388f
wearni <b.hoerterer@gmail.com>
2026-09-16 21:49:08 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,14 +13,14 @@
    ============================================================ */
 window.APP_CONFIG = {
   /* z. B. 'https://abcdefghijkl.supabase.co'  */
-  supabaseUrl: '',
+  supabaseUrl: 'https://xpaibmumgdjhfhmfettb.supabase.co',
 
   /* der lange "anon public"-Schlüssel */
-  supabaseKey: '',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwYWlibXVtZ2RqaGZobWZldHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1NzkyMzAsImV4cCI6MjEwNTE1NTIzMH0.a8xaLtka7QMrVREl3I12Cz2c013xd7xyROMmWUyMack',
 
   /* Name der Tabelle - muss zum SQL im README passen */
   table: 'highscores',
 
   /* Wie viele Einträge die Online-Liste zeigt */
-  topCount: 20
+  topCount: 40
 };
